318bb7a07d9a43f109e356b64af26ef268d514b4
spark1717 <sage.sypark@gmail.com>
2024-07-25 19:11:24 -0400

    First Commit

--- schema.sql
+++ schema.sql
@@ -1,40 +1,75 @@
+-- Table for product information 
 CREATE TABLE `products` (
     `id` INT AUTO_INCREMENT,
     `name` VARCHAR(100) NOT NULL,
-    `category` VARCHAR(50) NULL,
-    `Quantity` INT,
-    `price` DECIMAL(10,2) NOT NULL,
+    'description' TEXT,
+    `category` VARCHAR(50),
+    `price` DECIMAL(10, 2) NOT NULL,
+    `cost` DECIMAL(10, 2) NOT NULL,
     PRIMARY KEY(`id`)
-)
+);
 
+-- Table for supplier information 
 CREATE TABLE `suppliers` (
     `id` INT AUTO_INCREMENT,
-    `name` VARCHAR(100),
-    `contact` VARCHAR(100),
+    `name` VARCHAR(100) NOT NULL,
+    `contact_details` TEXT,
+    `address` TEXT,
     PRIMARY KEY(`id`)
-)
+);
 
-CREATE TABLE `customer` (
+-- M-M relationship between products and suppliers table 
+CREATE TABLE `ProductSuppliers` (
+        `product_id` INT, 
+        `supplier_id` INT,
+        PRIMARY KEY(`product_id`, `supplier_id`),
+        FOREIGN KEY(`product_id`) REFERENCES `products`(product_id),
+        FOREIGN KEY(`supplier_id`) REFERENCES `suppliers`(supplier_id)
+);
+
+-- Table for customer information 
+CREATE TABLE `customers` (
     `id` INT AUTO_INCREMENT,
-    `name` VARCHAR(30),
-    `contact` VARCHAR(100),
+    `name` VARCHAR(100) NOT NULL,
+    `contact_details` TEXT,
+    `address` TEXT,
     PRIMARY KEY(`id`)
-)
+);
 
+-- Table for order information 
 CREATE TABLE `orders` (
     `id` INT AUTO_INCREMENT,
-    `date` DATE,
     `customer_id` INT,
+    `order_date` DATE,
+    `status` VARCHAR(50) NOT NULL,
     PRIMARY KEY(`id`),
     FOREIGN KEY(`customer_id`) REFERENCES `customers`(`customer_id`)
-)
+);
 
-CREATE TABLE `transactions` (
+-- M-M relationship between orders and products table 
+CREATE TABLE `OrderDetails` (
+    `order_id` INT, 
+    `product_id` INT, 
+    `quantity` INT NOT NULL,
+    PRIMARY KEY(`order_id`, `product_id`),
+    FOREIGN KEY(`order_id`) REFERENCES `orders`(`order_id`),
+    FOREIGN KEY(`product_id`) REFERENCES `products`(`product_id`)
+);
+
+-- Table for warehouse information 
+CREATE TABLE `warehouses` (
+    `id` INT,
+    `name` TEXT NOT NULL,
+    `location` TEXT, 
+    `capacity` INTEGER
+);
+
+-- Table for managing inventory, linking products and warehouses 
+CREATE TABLE `inventory` (
     `id` INT,
     `product_id` INT,
-    `quantity` INT,
-    `date` DATE,
-    `type` VARCHAR(50),
-    PRIMARY KEY(`id`),
-    FOREIGN KEY(`product_id`) REFERENCES `products`(`id`)
-)
+    `warehouse_id` INT,
+    `quantity` INT NOT NULL,
+    FOREIGN KEY(`product_id`) REFERENCES `products`(`id`),
+    FOREIGN KEY(`warehouse_id`) REFERENCES `warehouses`(`warehouse_id`)
+);
